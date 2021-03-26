d58c3dabf804a0ff1ba71b24cc691f042fc27334
prasad <prasadzore31@gmail.com>
2021-03-26 16:37:15 +0530

    Basic Lexer Implemented

---
+++ cmake-build-debug/CMakeFiles/Base_Lang.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Base_Lang"
+  "Base_Lang.pdb"
+  "CMakeFiles/Base_Lang.dir/main.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Base_Lang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
